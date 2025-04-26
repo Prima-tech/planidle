70fe3611394a9b677182f388a37787a070bd343a
idleplanet9 <idleplanet9@gmail.com>
2025-04-26 13:17:30 +0200

    clean code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import Phaser from 'phaser';
-import { FakeApiService } from '../services/fakeapi';
-import { ProfileService } from '../services/profile';
-import { StaticData } from '../mocks/mock-inventory';
 
 @Component({
     selector: 'app-home',
@@ -11,19 +7,9 @@ import { StaticData } from '../mocks/mock-inventory';
     standalone: false,
 })
 export class HomePage  {
-    phaserGame: Phaser.Game | undefined;
-    items = StaticData.items;
 
-
-    constructor(
-     public service: FakeApiService,
-     public profile: ProfileService,
-    ) {
+    constructor( ) {
      
     }
-
-
-
-    
 
 }
